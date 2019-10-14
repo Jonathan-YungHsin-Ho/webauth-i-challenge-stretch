0ff34ef5ae163e5c7b594488a1038e296c40828d
Jonathan Ho <johnnyho82@gmail.com>
2019-10-14 17:55:05 -0400

    Set up components

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Nav from './components/Nav';
+import Login from './components/Login';
+import Register from './components/Register';
+import Users from './components/Users';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Users</h1>
+    </div>
+  );
+}
+
+export default App;
