3a62826bd608743f8b022c76f9832c3ed4b981a7
Jonathan Ho <johnnyho82@gmail.com>
2019-10-15 17:33:35 -0400

    Update header

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import './App.css';
 
-// import Nav from './components/Nav';
 import Login from './components/Login';
 import Register from './components/Register';
 import Users from './components/Users';
@@ -11,8 +10,7 @@ import PrivateRoute from './components/PrivateRoute';
 function App() {
   return (
     <div className='App'>
-      <h1>Users</h1>
-      {/* <Nav /> */}
+      <h1>Web Auth I Challenge React App</h1>
       <Route exact path='/'>
         <Redirect to='/login' />
       </Route>
